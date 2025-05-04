786e258241f02bf6450cf6a775443e4770539e30
Adil Aslam <aaliyanaslam22@gmail.com>
2025-05-04 12:00:37 +0500

    fix : signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-
-import Login from './pages/Login'
-import { Route , Routes } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import Signup from './pages/signup'
-
+import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
 
 function App() {
-
   return (
     <>
-  <Routes>
-  <Route path="/" element = {<Login/>} />
-  <Route path="/signup" element = {<Signup/>} />
-  <Route path="/dashboard" element = {<Dashboard/>} />
-
-  </Routes>
-     
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
